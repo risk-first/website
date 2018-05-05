7e65023f46492b9925eb425014a6fa31ca8ed3fe
Rob Moffat <robmoffat@mac.com>
2018-05-05 07:12:39 +0100

    Editing - Agency Risk

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
+export CLASSPATH=../website/target/classes
 
-
-java -classpath ../website/target/classes preprocessor.TextPreprocessor r1.md > r1-compiled.md
+java preprocessor.TextPreprocessor r1.md > r1-compiled.md
 
 pandoc -o r1.pdf r1-compiled.md --pdf-engine=xelatex
-# pandoc -t json r1-compiled.md | pandoc -f json -o r1.pdf --pdf-engine=xelatex
+#pandoc -t json r1-compiled.md | pandoc -f json -o r1.pdf --pdf-engine=xelatex
