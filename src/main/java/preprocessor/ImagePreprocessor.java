464bb58e02d46bfaa0041bc965784c1c58a5d563
Rob Moffat <robmoffat@mac.com>
2018-05-06 18:17:17 +0100

    Generating Images

--- src/main/java/preprocessor/ImagePreprocessor.java
+++ src/main/java/preprocessor/ImagePreprocessor.java
@@ -44,30 +44,35 @@ public class ImagePreprocessor {
 	}
 
 	private static void processGeneratedImage(String line) throws Exception {
-		String imagePath = line.substring(line.lastIndexOf("(")+1, line.lastIndexOf(".png)"));
-		String outputPath = "../website.wiki/"+imagePath+".png";
 		
-		// check if we have a template
-		TranscoderInput in = null;
-		File sourceFile = new File("../website.wiki/src/"+imagePath+".xml");
-		if (sourceFile.exists()) {
-			// a pre-existing file
-			in = new TranscoderInput(new FileReader(sourceFile));
-		} else {
-			// create one
-			String str = RepositoryHelp.stream(ImagePreprocessor.class.getResourceAsStream("icon-template.svg"));
-			String name = line.substring(line.lastIndexOf("/")+1, line.lastIndexOf("-risk."));
-			str = str.replaceAll("\\$\\{TYPE\\}", name);
-			in = new TranscoderInput(new StringReader(str));
+		try {
+			String imagePath = line.substring(line.lastIndexOf("(")+1, line.lastIndexOf(".png)"));
+			String outputPath = "../website.wiki/"+imagePath+".png";
+			
+			// check if we have a template
+			TranscoderInput in = null;
+			File sourceFile = new File("../website.wiki/src/"+imagePath+".xml");
+			if (sourceFile.exists()) {
+				// a pre-existing file
+				in = new TranscoderInput(new FileReader(sourceFile));
+			} else {
+				// create one
+				String str = RepositoryHelp.stream(ImagePreprocessor.class.getResourceAsStream("icon-template.svg"));
+				String name = line.substring(line.lastIndexOf("/")+1, line.lastIndexOf("-risk."));
+				str = str.replaceAll("\\$\\{TYPE\\}", name);
+				in = new TranscoderInput(new StringReader(str));
+			}
+			
+			TranscoderOutput out = new TranscoderOutput(new FileOutputStream(new File(outputPath)));
+			File kite9Dir = new File("kite9/somefile.svg");
+			in.setURI(kite9Dir.toURI().toString());
+			Kite9PNGTranscoder transcoder = new Kite9PNGTranscoder();
+			transcoder.addTranscodingHint(Kite9PNGTranscoder.KEY_PIXEL_UNIT_TO_MILLIMETER, .0635f);	// 400dpi
+			transcoder.transcode(in, out);
+			System.out.println("Wrote: "+outputPath);
+		} catch (Exception e) {
+			throw new Exception("Couldn't generate: "+line, e);
 		}
-		
-		TranscoderOutput out = new TranscoderOutput(new FileOutputStream(new File(outputPath)));
-		File kite9Dir = new File("kite9/somefile.svg");
-		in.setURI(kite9Dir.toURI().toString());
-		Kite9PNGTranscoder transcoder = new Kite9PNGTranscoder();
-		transcoder.addTranscodingHint(Kite9PNGTranscoder.KEY_PIXEL_UNIT_TO_MILLIMETER, .0635f);	// 400dpi
-		transcoder.transcode(in, out);
-		System.out.println("Wrote: "+outputPath);
 	}
 
 }
