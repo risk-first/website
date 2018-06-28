1efc43dd205229a6eda4ac3675a2450a24147f1c
Rob Moffat <robmoffat@mac.com>
2018-06-28 20:25:27 +0100

    Dependency Risk
    Former-commit-id: 86985c46b147cb9bab416388065237489bfe8b30

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 export CLASSPATH=../website/target/classes
 
-java preprocessor.TextPreprocessor r1.md > r1-compiled.md
+java org.riskfirst.website.preprocessor.TextPreprocessor r1.md > r1-compiled.md
 
 pandoc -o r1.pdf r1-compiled.md --pdf-engine=xelatex
 #pandoc -t json r1-compiled.md | pandoc -f json -o r1.pdf --pdf-engine=xelatex --dpi=400
