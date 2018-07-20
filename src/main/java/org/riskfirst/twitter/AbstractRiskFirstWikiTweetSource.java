1e5f2634866c8594c1520dd9ef9e13f4a9175b93
Rob Moffat <robmoffat@mac.com>
2018-07-20 14:34:42 +0100

    More tweeting

--- src/main/java/org/riskfirst/twitter/AbstractRiskFirstWikiTweetSource.java
+++ src/main/java/org/riskfirst/twitter/AbstractRiskFirstWikiTweetSource.java
@@ -3,15 +3,12 @@ package org.riskfirst.twitter;
 import java.net.URI;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.riskfirst.Article;
 import org.riskfirst.ArticleState;
 
-import twitter4j.StatusUpdate;
-
-public abstract class AbstractRiskFirstWikiTweetSource implements TweetSource {
+public abstract class AbstractRiskFirstWikiTweetSource extends AbstractTweetSource implements TweetSource {
 	
 	protected List<Article> articles;
 	protected URI baseUri;
@@ -21,17 +18,6 @@ public abstract class AbstractRiskFirstWikiTweetSource implements TweetSource {
 		this.articles = articles;
 	}
 
-	@Override
-	public List<StatusUpdate> getRandomTweets(int n) {
-		List<StatusUpdate> allTweets = getAllTweets();
-		Random r = new Random();
-		while (allTweets.size() > n) {
-			allTweets.remove(r.nextInt(allTweets.size()));
-		}
-
-		return allTweets;
-	}
-	
 	public List<Article> getArticlesInState(EnumSet<ArticleState> states) {
 		List<Article> out =  articles.stream().filter(a -> states.contains(a.getState())).collect(Collectors.toList());
 		return out;
