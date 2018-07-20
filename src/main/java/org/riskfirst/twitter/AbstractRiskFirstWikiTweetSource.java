6c49c4f333b011c44b66d51f9470d25bdcb8ff1b
Rob Moffat <robmoffat@mac.com>
2018-07-20 15:37:17 +0100

    More tweeting
    Former-commit-id: a00f2f7bcb08d5451a631e050b32ef973c75477f

--- src/main/java/org/riskfirst/twitter/AbstractRiskFirstWikiTweetSource.java
+++ src/main/java/org/riskfirst/twitter/AbstractRiskFirstWikiTweetSource.java
@@ -1,6 +1,7 @@
 package org.riskfirst.twitter;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,14 +13,22 @@ public abstract class AbstractRiskFirstWikiTweetSource extends AbstractTweetSour
 	
 	protected List<Article> articles;
 	protected URI baseUri;
+	protected List<String> hashtags;
 	
-	public AbstractRiskFirstWikiTweetSource(List<Article> articles, URI baseUri) {
+	public AbstractRiskFirstWikiTweetSource(List<Article> articles, URI baseUri, List<String> hashtags) {
 		this.baseUri = baseUri;
 		this.articles = articles;
+		this.hashtags = hashtags;
 	}
 
 	public List<Article> getArticlesInState(EnumSet<ArticleState> states) {
 		List<Article> out =  articles.stream().filter(a -> states.contains(a.getState())).collect(Collectors.toList());
 		return out;
+	}
+	
+	public String randomHashtags(int c) {
+		Collections.shuffle(hashtags);
+		List<String> sl = hashtags.subList(0, Math.min(c, hashtags.size()-1));
+		return " "+sl.stream().reduce((a, b) -> a+" "+b).orElse("");
 	}
 }
