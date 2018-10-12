9da8d1630f51e571379516e25b83b8b59a06dbf6
Rob Moffat <robmoffat@mac.com>
2018-10-12 10:53:45 +0100

    Improved link checker
    Former-commit-id: 5c75317f57e1902bfe4e71ffbb112bf1d44ad3d4

--- src/main/java/org/riskfirst/website/preprocessor/LinkChecker.java
+++ src/main/java/org/riskfirst/website/preprocessor/LinkChecker.java
@@ -3,9 +3,14 @@ package org.riskfirst.website.preprocessor;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -22,22 +27,24 @@ import org.kite9.framework.logging.Kite9Log;
  */
 public class LinkChecker {
 	
+	static String websitePrefix = null;
+	
 	static class Target implements Comparable<Target>{
 		
-		final String file;
+		final File file;
 		final int line;
 		final String link;
 		final String text;
 		final boolean image;
 		
-		public Target(String file, int line, String link, String text, boolean image) {
+		public Target(File file, int line, String link, String text, boolean image) {
 			super();
 			this.file = file;
 			this.line = line;
 			this.image = image;
 			
 			if (link.startsWith("#")) {
-				this.link = file.substring(0, file.lastIndexOf(".md")).toLowerCase()+link;
+				this.link = file.getName().substring(0, file.getName().lastIndexOf(".md")).toLowerCase()+link;
 			} else {
 				this.link = link;
 			}
@@ -47,7 +54,7 @@ public class LinkChecker {
 
 		@Override
 		public String toString() {
-			return "Target [file=" + file + ", line=" + line + ", link=" + link + ", text="+ text + "]";
+			return file + "," + line + "," + link + ","+ text;
 		}
 
 		@Override
@@ -67,54 +74,62 @@ public class LinkChecker {
 	}
 
 	public static void main(String[] args) throws Exception {
-		String file = args[0];
-		File f = new File(file);
-		
+		PrintStream bw = new PrintStream(new FileOutputStream(new File("links.csv")));
+		PrintStream tbw = new PrintStream(new FileOutputStream(new File("targets.csv")));
+
 		Kite9Log.setLogging(false);
-		
 		Set<String> links = new HashSet<>();
 		SortedSet<Target> targets = new TreeSet<>();
-		
-		process(f, links, targets);
+
+		for (String arg : args) {
+			if (websitePrefix == null) {
+				websitePrefix = arg;
+			} else {
+				File f = new File(arg);
+				process(f, links, targets, bw);
+			}
+		}
 		
 		Set<Target> externals = new HashSet<>();
 		
 		// now, we can remove targets that are ok
 		for (Iterator<Target> iterator = targets.iterator(); iterator.hasNext();) {
 			Target target = iterator.next();
-			if (target.image) {
-				if (new File(file, target.link).exists()) {
-					iterator.remove();
-				}
-			} else if (links.contains(target.link)) {
-				//System.out.println("FOUND: "+target);
+			if (links.contains(target.link)) {
+				//bw.println("FOUND,"+target);
 				iterator.remove();
 			} else if (target.link.startsWith("http")) {
 				externals.add(target);
 				iterator.remove();
+			} else {
+				bw.println("NOT_FOUND,"+target);
 			}
 		}
 		
-		for (Target target : targets) {
-			System.out.println("NOT_FOUND: "+target);
+		for (Target target : externals) {
+			//bw.println("EXTERNAL,"+target);
 		}
 		
-//		for (Target target : externals) {
-//			System.out.println("EXTERNAL: "+target);
-//		}
+		List<String> sorted = new ArrayList<>(links);
+		Collections.sort(sorted);
+		for (String link : sorted) {
+ 			tbw.println(link);
+		}
 		
 		System.out.println("Breaks: "+targets.size());
+		bw.close();
+		tbw.close();
 	}
 	
-	public static void process(File f, Set<String> links, Set<Target> targets) throws Exception {
+	public static void process(File f, Set<String> links, Set<Target> targets, PrintStream bw) throws Exception {
 		if (f.isDirectory()) {
 			for (File f2 : f.listFiles()) {
-				process(f2, links, targets);
+				process(f2, links, targets, bw);
 			}
 			
-		} else if (f.getName().endsWith(".md") && !f.getName().contains("compiled.md")) {
+		} else if (f.getName().endsWith(".md")) {
 			BufferedReader br = createBufferedReader(f);
-			links.add(createLink(f.getName(), null));
+			links.add(createMDLink(f.getName(), null));
 			int line = 0;
 			String content = null;
 			do {
@@ -122,23 +137,38 @@ public class LinkChecker {
 					// this only checks the one type of link like [dsfs](link).
 					TextPreprocessor.processLinks(content, 
 						(link, text, url, image, lineNo) -> {
-							targets.add(new Target(f.getName(), lineNo, url.toLowerCase(), text, image));
+							targets.add(new Target(f, lineNo, url.toLowerCase(), text, image));
 						}, line, (s) -> {});
 					
 					LinkChecker.processTargets(content, 
 						(title, depth, lineNo) -> {
-							String link = createLink(f.getName(), title);
+							String link = createMDLink(f.getName(), title);
 							links.add(link);
-						}, line);
+						}, line, bw);
 				}
 				content = br.readLine();
 				line++;
 			} while (content!=null);
 			
+		} else if (f.getName().endsWith(".png")) {
+			links.add(createImageLink(f.getPath()));
 		}
 	}
 	
-	private static String createLink(String name, String title) {
+	private static String createImageLink(String name) {
+		if (name.startsWith(websitePrefix)) {
+			String out = name.substring(websitePrefix.length());
+			return out.toLowerCase();
+		} else {
+			return name.toLowerCase();
+		}
+	}
+	
+	private static String createMDLink(String name, String title) {
+		if (name.startsWith(websitePrefix)) {
+			name = name.substring(websitePrefix.length());
+		}
+		
 		String mainName = name.substring(0,  name.indexOf(".md"));
 		if (title != null) {
 			String titleLink = title.trim().replaceAll("[\\&\\:\\/\\#]*", "").replace(" ","-");
@@ -147,7 +177,7 @@ public class LinkChecker {
 		
 		mainName = mainName.toLowerCase();
 
-		System.out.println("Link: "+mainName+" ");
+		//System.out.println("Link: "+mainName+" ");
 		
 		return mainName;
 	}
@@ -164,7 +194,7 @@ public class LinkChecker {
 		
 	}
 
-	public static void processTargets(String line, TargetProcessor lp, int lineNo) {
+	public static void processTargets(String line, TargetProcessor lp, int lineNo, PrintStream bw) {
 		Matcher m = p.matcher(line);
 		int place = 0;
 		while (m.find()) {
@@ -178,9 +208,9 @@ public class LinkChecker {
 			String reconstructed = hashes+" "+text;
 
 			if (!link.equals(reconstructed)) {
-				System.err.println("BAD: "+hashes+"    "+text+"   "+reconstructed);
+				System.err.println("BAD_TARGET,"+hashes+"    "+text+"   "+reconstructed);
 			} else {
-				System.err.println("OK: " + hashes+"    "+text+"    "+reconstructed);
+				System.out.println("TARGET," + hashes+"    "+text+"    "+reconstructed);
 			}
 			
 			lp.process(text, hashes.length(), lineNo);
