ef530794e7fba3a859345cc852992f6c225f4bfd
Rob Moffat <rob@kite9.com>
2024-01-31 08:37:10 +0000

    Removed temp folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/rob/Documents/risk-first/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/rob/Documents/risk-first/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/rob/Documents/risk-first/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/rob/Documents/risk-first/website/src/css/custom.css"),
-];
