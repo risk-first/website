1ade75010216838fbf0499a491f59df9da982571
Rob Moffat <rob@kite9.com>
2024-02-07 09:14:45 +0000

    Fixed font, theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,135 +4,135 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Risk First',
-  tagline: 'Exploring the risks that ruin software projects',
-  favicon: 'img/favicon.ico',
+	title: 'Risk First',
+	tagline: 'Exploring the risks that ruin software projects',
+	favicon: 'img/favicon/apple-icon-180x180.png',
 
-  // Set the production url of your site here
-  url: 'https://riskfirst.org',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+	// Set the production url of your site here
+	url: 'https://riskfirst.org',
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'facebook', // Usually your GitHub org/user name.
+	projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" wrouteith "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" wrouteith "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-		  routeBasePath: '/',
-          sidebarPath: './sidebars.js',
-          remarkPlugins: [remarkMath],
-          rehypePlugins: [rehypeKatex],
-          editUrl:
-            'https://github.com/risk-first/website/blon/master/',
-        },
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					routeBasePath: '/',
+					sidebarPath: './sidebars.js',
+					remarkPlugins: [remarkMath],
+					rehypePlugins: [rehypeKatex],
+					editUrl:
+						'https://github.com/risk-first/website/blon/master/',
+				},
+				blog: false,
+				theme: {
+					customCss: './src/css/custom.css',
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Risk First',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/thinking', label: 'Thinking', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			// Replace with your project's social card
+			image: 'img/docusaurus-social-card.jpg',
+			navbar: {
+				title: 'Risk First',
+				logo: {
+					alt: 'Risk First Logo',
+					src: 'img/R1_logo_grue.svg',
+				},
+				items: [
+					{ to: '/overview/Start', label: 'Overview', position: 'left' },
+					{ to: '/thinking/Start', label: 'Thinking', position: 'left' },
+					{ to: '/risks/Start', label: 'Risks', position: 'left' },
+					{ to: '/estimating/Start', label: 'Estimating', position: 'left' },
+					{ to: '/complexity/Start', label: 'Complexity', position: 'left' },
+					{
+						href: 'https://github.com/risk-first/website',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{ to: '/overview/Start', label: 'Overview', position: 'left' },
+							{ to: '/thinking/Start', label: 'Thinking', position: 'left' },
+							{ to: '/risks/Start', label: 'Risks', position: 'left' },
+							{ to: '/estimating/Start', label: 'Estimating', position: 'left' },
+							{ to: '/complexity/Start', label: 'Complexity', position: 'left' },
+						],
+					},
+					{
+						title: 'Further Info',
+						items: [
+							{
+								label: 'RiskFirst GitHub Team (News)',
+								href: 'https://github.com/orgs/risk-first/teams/risk-first-team',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/risk-first/website',
+							},
+							{
+								label: 'Rob On LinkedIn',
+								href: 'https://www.linkedin.com/in/robmoffat/',
+							},
+						],
+					},
+					{
+						title: 'GitHub',
+						items: [
+							{
+								html: '<a href="https://github.com/risk-first/website/stargazers"><img src="https://img.shields.io/github/stars/risk-first/website" alt="Github Stars"/></a>',
+							},
+							{
+								html: '<a href="https://github.com/risk-first/website/forks"><img src="https://img.shields.io/github/forks/risk-first/website" alt="Github Forks"/></a>',
+							},
+							{
+								html: '<a href="https://github.com/risk-first/website/watchers"><img src="https://img.shields.io/github/watchers/risk-first/website" alt="Github Watchers"/></a>',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Kite9 Ltd. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: prismThemes.github,
+				darkTheme: prismThemes.dracula,
+			},
+		}),
 };
 
 export default config;
