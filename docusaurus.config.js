c46a889449d22abd122b604f5895bc6a077ddebc
Rob Moffat <rob@kite9.com>
2024-02-10 21:35:51 +0000

    Fixing gh actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: 'risk-first', // Usually your GitHub org/user name.
 	projectName: 'website', // Usually your repo name.
-	deploymentBranch: 'main',
+	deploymentBranch: 'master',
 
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
