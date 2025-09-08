d736298d986397e0ef819cb3cfb6fc34d6a7e539
Rob Moffat <rob@kite9.com>
2025-09-08 14:15:38 +0100

    Added details of second edition to home page

--- src/components/front/Books/index.js
+++ src/components/front/Books/index.js
@@ -7,20 +7,26 @@ import TagList from '../../../theme/TagList'
 
 export default function Books() {
 	return (
-		<HomeSection reversed="true">
-			<div className={styles.innerAside}>
-				<h2>Read All About It</h2>
-				<p>Buy The Print Edition of Risk-First</p>
-			</div>
-			<>
-				<a href="/The-Menagerie"><img src="/img/Cover_Image_Bordered.png" /></a>
-			</>
-			<div>
-				<p><a className={styles.cta} href="/The-Menagerie">BUY THE BOOK</a></p>
-			</div>
-		</HomeSection>
-
-
+		<>
+			<HomeSection reversed="false">
+				<>
+					<a href="/Risk-First-Second-Edition"><img src="/img/Cover_Image_Second_Edition.jpg" /></a>
+				</>
+				<div className={styles.innerAside}>
+					<h2>Second Edition <strong>beta</strong></h2>
+					<p>Now Available in <a href="/Risk-First-Second-Edition">Digital Format!</a></p>
+				</div>
+			</HomeSection>
+			<HomeSection reversed="true">
+				<div className={styles.innerAside}>
+					<h2>Read All About It</h2>
+					<p>Buy The <a href="/The-Menagerie">Original Print Edition</a> of Risk-First</p>
+				</div>
+				<>
+					<a href="/The-Menagerie"><img src="/img/Cover_Image_Bordered.png" /></a>
+				</>
+			</HomeSection>
+		</>
 	)
 }
 
